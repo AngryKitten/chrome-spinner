0bc60034634b1cf66edb3867c587875a04597403
E2ML18225 <caleb@Slab.local>
2019-06-28 11:56:16 -0400

    First and last commit

---
+++ README.md
@@ -0,0 +1,11 @@
+To use:
+
+1. Clone or download the repo
+
+2. Go to your extensions list in Chrome and click Load unpacked
+
+3. Navigate to the repo folder and select it
+
+4. Activate it and refresh or navigate to any site
+
+5. Hover elements and listen to your browser cry
---
+++ spinner.js
@@ -0,0 +1,15 @@
+const elems = [...document.querySelectorAll('*:not(html):not(body)')];
+
+const rand = (min, max, mod) => {
+  return Math.floor(Math.random() * max + min) * mod;
+};
+
+const spin = (elem) => {
+  elem.addEventListener('mouseover', () => {
+    elem.style.color = 'firebrick';
+    elem.style.transition = elem.style.transition || `all ${rand(5, 100, 1)}s ease-in-out`;
+    elem.style.transform = elem.style.transform || `rotate(${rand(-5, 11, 360)}deg)`;
+  });
+};
+
+elems.map(elem => spin(elem));
